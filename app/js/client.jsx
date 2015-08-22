95c6a200adbb93714ea2a0e9474e9aa45b7445d1
Andrew Jacobson <andyjacuw@gmail.com>
2015-08-22 13:38:16 -0700

    Request photo data from API

--- app/js/client.jsx
+++ app/js/client.jsx
@@ -1,11 +1,31 @@
 var React = require('react');
+var request = require('superagent');
+var PHOTO_URL = 'https://appsheettest1.azurewebsites.net/sample/posts';
 
 var App = React.createClass({
   getInitialState: function() {
     return { photos: [] };
   },
 
+  componentDidMount: function() {
+    this.loadPhotos();
+  },
+
+  loadPhotos: function() {
+    request
+      .get(PHOTO_URL)
+      .end(function(err, res) {
+        if (err) {
+          return console.log(err);
+        }
+
+        this.setState({photos: res.body});
+      }.bind(this));
+  },
+
   render: function() {
+    console.log(this.state.photos);
+
     return (
       <main className="main">
         <h1>Hello World!</h1>
